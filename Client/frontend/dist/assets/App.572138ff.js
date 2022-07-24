efb931513c3a954bcb8a4631aaef600f483cd957
Antonio Cheong <acheong@student.dalat.org>
2022-07-24 18:41:40 +0800

    Delete distribution files

--- Client/frontend/dist/assets/App.572138ff.js
+++
@@ -1,1 +0,0 @@
-function o(n,e){return window.go.main.App.Devices(n,e)}function r(n,e){return window.go.main.App.Login(n,e)}function p(n,e,i){return window.go.main.App.Reserve(n,e,i)}export{o as D,r as L,p as R};
